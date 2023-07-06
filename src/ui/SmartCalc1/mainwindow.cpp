5a4cd4a5fee95fe45dac527f4671a548ee5be3ff
Hasuk1 <92762015+Hasuk1@users.noreply.github.com>
2023-07-06 06:43:32 +0300

    Add files via upload

---
+++ src/ui/SmartCalc1/mainwindow.cpp
@@ -0,0 +1,67 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle("SmartCalc");
+    setFixedSize(width(), height());
+
+    connect(ui->PushButton_0, SIGNAL(clicked()), this, SLOT(digits_num()));
+    connect(ui->PushButton_1, SIGNAL(clicked()), this, SLOT(digits_num()));
+    connect(ui->PushButton_2, SIGNAL(clicked()), this, SLOT(digits_num()));
+    connect(ui->PushButton_3, SIGNAL(clicked()), this, SLOT(digits_num()));
+    connect(ui->PushButton_4, SIGNAL(clicked()), this, SLOT(digits_num()));
+    connect(ui->PushButton_5, SIGNAL(clicked()), this, SLOT(digits_num()));
+    connect(ui->PushButton_6, SIGNAL(clicked()), this, SLOT(digits_num()));
+    connect(ui->PushButton_7, SIGNAL(clicked()), this, SLOT(digits_num()));
+    connect(ui->PushButton_8, SIGNAL(clicked()), this, SLOT(digits_num()));
+    connect(ui->PushButton_9, SIGNAL(clicked()), this, SLOT(digits_num()));
+    connect(ui->PushButton_del, SIGNAL(clicked()), this, SLOT(delete_char()));
+    connect(ui->PushButton_all_del, SIGNAL(clicked()), this, SLOT(delete_text()));
+    connect(ui->PushButton_dot, SIGNAL(clicked()), this, SLOT(on_PushButton_dot_clicked()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::digits_num()
+{
+    QPushButton *button = static_cast<QPushButton*>(sender());
+    QString currentText = ui->Labe_out->text();
+
+    // Проверяем, является ли текущий текст "0"
+    if (currentText == "0" && button->text() != "0") {
+        ui->Labe_out->setText(button->text());
+    } else {
+        ui->Labe_out->setText(currentText + button->text());
+    }
+}
+
+void MainWindow::delete_char()
+{
+    QString currentText = ui->Labe_out->text();
+    if (!currentText.isEmpty()) {
+        currentText.chop(1);
+        ui->Labe_out->setText(currentText);
+    }
+    else {
+        ui->Labe_out->setText("0");
+    }
+}
+
+void MainWindow::delete_text()
+{
+    ui->Labe_out->clear();
+    ui->Labe_out->setText("0");
+}
+
+void MainWindow::on_PushButton_dot_clicked()
+{
+    if(!(ui->Labe_out->text().contains('.')))
+        ui->Labe_out->setText(ui->Labe_out->text() + ".");
+}
